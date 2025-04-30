df1ef3c54ca8daac603fa13f006463342695510c
Serhii Zashchelkin <serg.zz@ukr.net>
2025-04-30 16:45:39 +0200

    .NET client draft

---
+++ dotNET/FileServiceClientLibrary/FileService.cs
@@ -0,0 +1,75 @@
+﻿using NetworkServiceClientLibrary;
+
+namespace FileServiceClientLibrary;
+
+internal enum RequestId {
+    Get,
+    Set,
+    GetLast,
+    FetFileVersion
+}
+
+public record File(int Version, byte[] Data);
+
+public record GetResponse(int DbVersion, Dictionary<int, File> Data);
+
+public record GetLastResponse(int DbVersion, int Key, File Data);
+
+public record FileServiceConfig(int UserId, byte[] Key, string HostName, short Port, int TimeoutMs, string DbName);
+
+public class FileService(FileServiceConfig config): NetworkService(new NetworkServiceConfig(
+    BitConverter.GetBytes(config.UserId),
+    config.Key,
+    config.HostName,
+    config.Port,
+    config.TimeoutMs))
+{
+    private readonly string _dbName = config.DbName;
+
+    public GetResponse Get(int key1, int key2)
+    {
+        var request = BuildGetRequest(RequestId.Get, key1, key2);
+        var response = Send(request);
+        return response[0] switch
+        {
+            //no error
+            0 => DecodeGetResponse(response[1..]),
+            // error
+            _ => throw new ResponseError(response)
+        };
+    }
+
+    private byte[] BuildGetRequest(RequestId requestId, int key1, int key2)
+    {
+        var bytes = System.Text.Encoding.UTF8.GetBytes(_dbName);
+        using var stream = new MemoryStream();
+        using var bw = new BinaryWriter(stream);
+        bw.Write((byte)requestId);
+        bw.Write((byte)bytes.Length);
+        bw.Write(bytes);
+        bw.Write(key1);
+        bw.Write(key2);
+        return stream.ToArray();
+    }
+
+    private static GetResponse DecodeGetResponse(byte[] bytes)
+    {
+        using var reader = new BinaryReader(new MemoryStream(bytes));
+        var dbVersion = reader.ReadInt32();
+        var length = reader.ReadInt32();
+        var result = new Dictionary<int, File>();
+        while (length-- > 0)
+        {
+            var fileVersion = reader.ReadInt32();
+            var key = reader.ReadInt32();
+            var valueLength = reader.ReadInt32();
+            var value = reader.ReadBytes(valueLength);
+            result[key] = new File(fileVersion, value);
+        }
+
+        if (reader.BaseStream.Position != reader.BaseStream.Length)
+            throw new IOException("Incorrect response length");
+        
+        return new GetResponse(dbVersion, result);
+    }
+}
---
+++ dotNET/NetworkServiceClientLibrary/NetworkService.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace NetworkServiceClientLibrary;
+
+public record NetworkServiceConfig(
+    byte[] Prefix,
+    byte[] Key,
+    string HostName,
+    short Port,
+    int TimeoutMs = 1000);
+
+public class NetworkService
+{
+    private readonly UdpClient _udpClient;
+    private readonly NetworkServiceConfig _config;
+    
+    public NetworkService(NetworkServiceConfig config)
+    {
+        _config = config;
+        _udpClient = new UdpClient(config.HostName, config.Port);
+        _udpClient.Client.ReceiveTimeout = config.TimeoutMs;
+    }
+
+    public byte[] Send(byte[] request)
+    {
+        using var stream = new MemoryStream();
+        using var bw = new BinaryWriter(stream);
+        bw.Write(_config.Prefix);
+        bw.Write(Encrypt(request));
+        var data = stream.ToArray();
+        var response = SendUdp(data);
+        return Decrypt(response);
+    }
+    
+    private byte[] SendUdp(byte[] data)
+    {
+        _udpClient.Send(data, data.Length);
+        var ep = new IPEndPoint(IPAddress.Any, 0);
+        return _udpClient.Receive(ref ep);
+    }
+
+    private byte[] Encrypt(byte[] data)
+    {
+        var iv = BuildIv();
+        var transformed = TransformIv(iv);
+        var cipher = new ChaCha20(_config.Key, iv);
+        var encrypted = cipher.Encrypt(data);
+        using var stream = new MemoryStream();
+        using var bw = new BinaryWriter(stream);
+        bw.Write(transformed);
+        bw.Write(encrypted);
+        return stream.ToArray();
+    }
+
+    private byte[] Decrypt(byte[] data)
+    {
+        if (data.Length < 13)
+            throw new IOException("Invalid response.");
+        var iv = TransformIv(data[..12]);
+        var cipher = new ChaCha20(_config.Key, iv);
+        return cipher.Encrypt(data[12..]);
+    }
+    
+    private byte[] TransformIv(byte[] iv)
+    {
+        using var stream = new MemoryStream();
+        using var bw = new BinaryWriter(stream);
+        bw.Write(iv[..4]);
+        bw.Write(iv[..4]);
+        bw.Write(iv[..4]);
+        var iv3 = stream.ToArray();
+        var cipher = new ChaCha20(_config.Key, iv3);
+        var transformed = cipher.Encrypt(iv[4..]);
+        Array.Copy(transformed, 0, iv3, 4, 8);
+        return iv3;
+    }
+    
+    private static byte[] BuildIv()
+    {
+        using var stream = new MemoryStream();
+        using var bw = new BinaryWriter(stream);
+        bw.Write(RandomNumberGenerator.GetBytes(4));
+        bw.Write(((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds());
+        return stream.ToArray();
+    }
+}
+
+public class ResponseError(byte[] decompressed): IOException(BuildMessage(decompressed))
+{
+    private static string BuildMessage(byte[] decompressed)
+    {
+        if (decompressed[0] == 2)
+        {
+            var message = System.Text.Encoding.UTF8.GetString(decompressed[1..]);
+            return $"Error: {message}";
+        }
+
+        return $"Wrong response type {decompressed[0]}";
+    }
+}
